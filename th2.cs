06eb85657c0da509b4b019b81f2a57e8add2a061
TungNguyenAnh <114374972+TungNguyenAnh@users.noreply.github.com>
2024-04-15 14:03:15 +0700

    Create th2.cs

---
+++ th2.cs
@@ -0,0 +1,20 @@
+﻿int[] mangSo = { 5, 2, 4, 1, 3 };
+int viTriSoNhoNhat = TimIndexSoNhoNhat(mangSo);
+Console.WriteLine("Chỉ mục của số nhỏ nhất: " + viTriSoNhoNhat);
+
+int TimIndexSoNhoNhat(int[] mangSo)
+{
+    int viTriSoNhoNhat = 0;
+    int soNhoNhat = mangSo[0];
+
+    for (int i = 1; i < mangSo.Length; i++)
+    {
+        if (mangSo[i] < soNhoNhat)
+        {
+            soNhoNhat = mangSo[i];
+            viTriSoNhoNhat = i;
+        }
+    }
+
+    return viTriSoNhoNhat;
+}
