1c31b5f0e38489ef919b4ec74d04b8e49e4b7b95
TungNguyenAnh <114374972+TungNguyenAnh@users.noreply.github.com>
2024-04-16 09:42:21 +0700

    Create bt1.cs

---
+++ bt1.cs
@@ -0,0 +1,26 @@
+﻿int[] arrays = { 10, 20, 30, 40, 50 };
+
+System.Console.WriteLine("where to remove?:");
+int index = int.Parse(Console.ReadLine());
+int[] resultArrays = new int[arrays.Length - 1]; // Tạo mảng mới với kích thước nhỏ hơn 1
+
+// Sao chép các phần tử sang mảng mới, bỏ qua phần tử tại vị trí được chọn
+int j = 0;
+for (int i = 0; i < arrays.Length; i++)
+{
+    if (i != index)
+    {
+        resultArrays[j] = arrays[i];
+        j++;
+    }
+}
+
+// Gán mảng mới cho mảng cũ
+arrays = resultArrays;
+
+// In mảng sau khi xóa
+Console.WriteLine("Result Arrays:");
+foreach (int number in arrays)
+{
+    Console.Write(number + " ");
+}
