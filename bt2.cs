7ec9976c80179780646d2d7d9eac3724a8020ed8
TungNguyenAnh <114374972+TungNguyenAnh@users.noreply.github.com>
2024-04-16 10:31:43 +0700

    Create bt2.cs

---
+++ bt2.cs
@@ -0,0 +1,20 @@
+﻿string text = "hello world";
+
+Console.WriteLine("Nhập chuỗi: ");
+string type = Console.ReadLine();
+
+int count = 0;
+
+for (int i = 0; i < type.Length; i++)
+{
+    for (int j = 0; j < text.Length; j++)
+    {
+        if (type[i] == text[j])
+        {
+            count++;
+            break;
+        }
+    }
+}
+
+Console.WriteLine("Số lần trùng khớp: {0}", count);
